dfe6f202edd146534fbabebcded8c82408a4732d
gavan <gavan.jon.lamb@gmail.com>
2021-08-08 13:19:16 +1000

    Add custom config

--- src/Time.Api/Program.cs
+++ src/Time.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,9 +17,37 @@ namespace Time.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
+                    config.Sources.Clear();
+                    
+                    var env = hostingContext.HostingEnvironment;
+
+                    var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
+
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: reloadOnChange);
+                        
                     if (hostingContext.HostingEnvironment.EnvironmentName.StartsWith("Preview"))
                     {
-                        config.AddJsonFile("appsettings.Preview.json", true, true);
+                        config.AddJsonFile("appsettings.Preview.json", false, true);
+                    }
+                    else
+                    {
+                        config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: reloadOnChange);
+                    }
+                    
+                    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
+                    {
+                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                        if (appAssembly != null)
+                        {
+                            config.AddUserSecrets(appAssembly, optional: true);
+                        }
+                    }
+
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
                     }
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
