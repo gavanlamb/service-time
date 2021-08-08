6f54dec9d545cd256e142141b3ece7e69057cd80
gavan <gavan.jon.lamb@gmail.com>
2021-08-08 15:51:19 +1000

    test

--- src/Time.Api/Program.cs
+++ src/Time.Api/Program.cs
@@ -15,41 +15,6 @@ namespace Time.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.Sources.Clear();
-                    
-                    var env = hostingContext.HostingEnvironment;
-
-                    var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
-
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: reloadOnChange);
-                        
-                    if (hostingContext.HostingEnvironment.EnvironmentName.StartsWith("Preview"))
-                    {
-                        config.AddJsonFile("appsettings.Preview.json", false, reloadOnChange);
-                    }
-                    else
-                    {
-                        config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: reloadOnChange);
-                    }
-                    
-                    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
-                    {
-                        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
-                        if (appAssembly != null)
-                        {
-                            config.AddUserSecrets(appAssembly, optional: true);
-                        }
-                    }
-
-                    config.AddEnvironmentVariables();
-
-                    if (args != null)
-                    {
-                        config.AddCommandLine(args);
-                    }
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
