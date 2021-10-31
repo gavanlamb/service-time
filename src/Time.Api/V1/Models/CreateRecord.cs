1b731822b2d7b634ad3e746d3ac9d1ad77b548ba
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-10-31 18:04:00 +1100

    Add documentation to request models

--- src/Time.Api/V1/Models/CreateRecord.cs
+++ src/Time.Api/V1/Models/CreateRecord.cs
@@ -5,15 +5,15 @@ namespace Time.Api.V1.Models
     public class CreateRecord
     {
         /// <summary>
-        /// 
+        /// Name of the time record
         /// </summary>
-        /// <example></example>
+        /// <example>Meeting</example>
         public string Name { get; init; }
         
         /// <summary>
-        /// 
+        /// Time the record started
         /// </summary>
-        /// <example></example>
+        /// <example>2021-10-31T04:46:32.3044710Z</example>
         public DateTime Start { get; init; }
     }
 }
