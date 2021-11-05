fd69514f80ea91ca4b0db30602208325724545ce
gavan <gavan.jon.lamb@gmail.com>
2021-11-05 20:24:22 +1100

    Refactor record controller

--- src/Time.Api/V1/Controllers/RecordController.cs
+++ src/Time.Api/V1/Controllers/RecordController.cs
@@ -1,6 +1,10 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Threading.Tasks;
+using AutoMapper;
+using Expensely.Authentication.Cognito.Jwt.Extensions;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -8,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Time.Api.V1.Models;
 using Time.Domain.Commands.Records;
+using Time.Domain.Queries.Records;
 
 namespace Time.Api.V1.Controllers
 {
@@ -18,16 +23,24 @@ namespace Time.Api.V1.Controllers
     public class RecordController : ControllerBase
     {
         private readonly ILogger<RecordController> _logger;
+        private readonly IMapper _mapper;
         private readonly IMediator _mediatr;
 
         public RecordController(
             ILogger<RecordController> logger,
+            IMapper mapper,
             IMediator mediatr)
         {
             _logger = logger;
+            _mapper = mapper;
             _mediatr = mediatr;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="createRecord"></param>
+        /// <returns></returns>
         [HttpPost]
         [Authorize("create")]
         [ProducesResponseType(StatusCodes.Status201Created)]
@@ -38,100 +51,153 @@ namespace Time.Api.V1.Controllers
         public async Task<ActionResult<Record>> Post(
             [FromBody] CreateRecord createRecord)
         {
-            var command = new CreateRecordCommand
-            {
-                Name = createRecord.Name,
-                Start = createRecord.Start,
-                UserId = "user id"
-            };
-            var record = await _mediatr.Send(command);
+            var userId = HttpContext.User.GetSubject();
+            
+            var command = _mapper.Map<CreateRecordCommand>(createRecord, opt => opt.Items["UserId"] = userId);
+            
+            var recordDomain = await _mediatr.Send(command);
+            
+            var record = _mapper.Map<Record>(recordDomain);
             
             return CreatedAtAction(nameof(Get), new { id = record.Id }, record);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="updateRecord"></param>
+        /// <returns></returns>
         [HttpPut("{id:long}")]
         [Authorize("update")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<Record>> Put(
             [FromRoute] long id,
             [FromBody] UpdateRecord updateRecord)
         {
-            var command = new UpdateRecordCommand
-            {
-                Id = id,
-                Name = updateRecord.Name,
-                Start = updateRecord.Start,
-                End = updateRecord.End,
-                UserId = "user id"
-            };
-            var record = await _mediatr.Send(command);
+            var userId = HttpContext.User.GetSubject();
+            
+            var command = _mapper.Map<UpdateRecordCommand>(
+                updateRecord, 
+                opt =>
+                {
+                    opt.Items["UserId"] = userId;
+                    opt.Items["Id"] = id;
+                });
+            
+            var recordDomain = await _mediatr.Send(command);
+            
+            var record = _mapper.Map<Record>(recordDomain);
+
             return Ok(record);
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id:long}")]
         [Authorize("delete")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<Record>> Delete(
             [FromRoute] long id)
         {
-            var command = new DeleteRecordCommand
-            {
-                Id = id,
-                UserId = "user id"
-            };
+            var userId = HttpContext.User.GetSubject();
+            
+            var command = _mapper.Map<DeleteRecordCommand>(id, opt => opt.Items["UserId"] = userId);
+
             await _mediatr.Send(command);
+            
             return Ok();
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id:long}")]
         [Authorize("read")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<Record>> Get(
             [FromRoute] long id)
         {
-            try
+            var userId = HttpContext.User.GetSubject();
+            
+            var query = _mapper.Map<GetRecordByIdQuery>(id, opt => opt.Items["UserId"] = userId);
+            
+            var result = await _mediatr.Send(query);
+            if (result == null)
             {
-                return NoContent();
+                return NotFound();
             }
-            catch (Exception e)
-            {
-                _logger.LogError(
-                    e, 
-                    "Error encountered while getting records for user"); 
-
-                return StatusCode(500);
-            }
+            
+            var record = _mapper.Map<Record>(result);
+            
+            return Ok(record);
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Authorize("read")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<IEnumerable<Record>>> Fetch()
+        [ProducesErrorResponseType(typeof(void))]
+        public async Task<ActionResult<IEnumerable<Record>>> Fetch(
+            [FromQuery] int pageNumber,
+            [FromQuery] int PageSize)
         {
-            try
+            var userId = HttpContext.User.GetSubject();
+            
+            var query = new GetRecordsQuery
             {
+                PageNumber = pageNumber,
+                PageSize = PageSize,
+                UserId = userId
+            };
+            
+            var result = await _mediatr.Send(query);
+            if (!result.Items.Any())
                 return NoContent();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(
-                    e, 
-                    "Error encountered while getting records for user"); 
 
-                return StatusCode(500);
-            }
+            var records = _mapper.Map<IEnumerable<Record>>(result.Items);
+
+            var paginationDetails = Helpers.Pagination.GetPaginationDetails(
+                Url,
+                result,
+                nameof(Fetch));
+            
+            Response.Headers.Add(
+                "X-Pagination", 
+                JsonSerializer.Serialize(
+                    paginationDetails,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        IgnoreNullValues = true,
+                        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                    })
+                );
+
+            return Ok(records);
         }
     }
 }
