e35b8e7e2b5d51ca4c2edf7a0317eec93630c046
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-11-11 17:40:54 +1100

    Update test listener port value

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -36,7 +36,7 @@ data "aws_lb_listener" "expensely_https" {
 }
 data "aws_lb_listener" "expensely_test" {
   load_balancer_arn = data.aws_lb.expensely.arn
-  port = 8000
+  port = 8443
 }
 
 data "aws_kms_alias" ssm_default_key{
