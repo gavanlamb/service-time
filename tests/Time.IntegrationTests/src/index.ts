148f0077d5dad7ce96cdd25cc83906b0b6c11bfc
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-11-11 22:12:52 +1100

    Wrap function in try catch

--- tests/Time.IntegrationTests/src/index.ts
+++ tests/Time.IntegrationTests/src/index.ts
@@ -16,60 +16,64 @@ const buildNumber = process.env.BUILD_NUMBER as string;
 const resultsBucket = process.env.RESULTS_BUCKET as string;
 const baseUrl = process.env.BASEURL as string;
 
-export const handler = async (event: any, context: any) => {
-    const codeDeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
-    const deploymentId = event.DeploymentId;
-    const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
-    const resultsFile = `results.${buildNumber}.xml`;
-    const environmentConfiguration = environment.startsWith("Preview") ? variableMap["Preview"] : variableMap[environment]
-    await run(
-        {
-            // @ts-ignore
-            abortOnFailure: true,
-            collection: apiCollection,
-            envVar: [
+export const handler = async (event: any, context: any): Promise<void> => {
+    try {
+        const codeDeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
+        const deploymentId = event.DeploymentId;
+        const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+        const resultsFile = `results.${buildNumber}.xml`;
+        const environmentConfiguration = environment.startsWith("Preview") ? variableMap["Preview"] : variableMap[environment]
+        run(
+            {
                 // @ts-ignore
-                {
-                    "key": "baseUrl",
-                    "value": baseUrl
-                }
-            ],
-            environment: environmentConfiguration,
-            reporters: ['cli', 'junitfull'],
-            reporter: {
-                junitfull: {
-                    export: `/tests/${resultsFile}`,
+                abortOnFailure: true,
+                collection: apiCollection,
+                envVar: [
+                    // @ts-ignore
+                    {
+                        "key": "baseUrl",
+                        "value": baseUrl
+                    }
+                ],
+                environment: environmentConfiguration,
+                reporters: ['cli', 'junitfull'],
+                reporter: {
+                    junitfull: {
+                        export: `/tests/${resultsFile}`,
+                    },
                 },
             },
-        },
-        async (error: any, data: any) => {
-            if (resultsBucket) {
-                const s3 = new aws.S3();
-                const testResultsData = fs.readFileSync(`/tests/${resultsFile}`, 'utf8');
-                await s3.upload({
-                    ContentType: "application/xml",
-                    Bucket: resultsBucket,
-                    Body: testResultsData,
-                    Key: `/${environment}/Time.Api/${resultsFile}`
-                }).promise()
+            async (error: any, data: any) => {
+                if (resultsBucket) {
+                    const s3 = new aws.S3();
+                    const testResultsData = fs.readFileSync(`/tests/${resultsFile}`, 'utf8');
+                    await s3.upload({
+                        ContentType: "application/xml",
+                        Bucket: resultsBucket,
+                        Body: testResultsData,
+                        Key: `/${environment}/Time.Api/${resultsFile}`
+                    }).promise()
+                }
+                if (error) {
+                    console.error(error)
+                    const params = {
+                        deploymentId,
+                        lifecycleEventHookExecutionId,
+                        status: 'Failed'
+                    };
+                    await codeDeploy.putLifecycleEventHookExecutionStatus(params).promise();
+                } else {
+                    console.log(data)
+                    const params = {
+                        deploymentId,
+                        lifecycleEventHookExecutionId,
+                        status: 'Succeeded'
+                    };
+                    await codeDeploy.putLifecycleEventHookExecutionStatus(params).promise();
+                }
             }
-            if (error) {
-                console.error(error)
-                const params = {
-                    deploymentId,
-                    lifecycleEventHookExecutionId,
-                    status: 'Failed'
-                };
-                await codeDeploy.putLifecycleEventHookExecutionStatus(params).promise();
-            } else {
-                console.log(data)
-                const params = {
-                    deploymentId,
-                    lifecycleEventHookExecutionId,
-                    status: 'Succeeded'
-                };
-                await codeDeploy.putLifecycleEventHookExecutionStatus(params).promise();
-            }
-        }
-    );
+        );
+    } catch (error) {
+        console.error(error);
+    }
 }
