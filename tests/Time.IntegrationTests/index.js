a8d9b352645634990f2dc811f9cb7fdb4bbf33e3
Gavan Lamb <gavan.jon.lamb@gmail.com>
2021-11-12 14:39:46 +1100

    remove try catch

--- tests/Time.IntegrationTests/index.js
+++ tests/Time.IntegrationTests/index.js
@@ -30,52 +30,65 @@ exports.handler = (event, context, callback) => {
                 },
             },
         },
-        (error, data) => {
+        (newmanError, newmanData) => {
+            
+            if (newmanError) {
+                console.error(newmanError);
+            } else if (newmanData) {
+                console.log(newmanData);
+            }
+            
             if (resultsBucket) {
                 const s3 = new aws.S3();
                 const testResultsData = fs.readFileSync(`/tmp/${resultsFile}`, 'utf8');
-                console.log(testResultsData);
-                s3.upload({
-                    ContentType: "application/xml",
-                    Bucket: resultsBucket,
-                    Body: testResultsData,
-                    Key: `/${environment}/Time.Api/${resultsFile}`
-                });
-            }
-            if (error) {
-                console.error(error);
-                const params = {
-                    deploymentId: event.DeploymentId,
-                    lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
-                    status: 'Failed'
-                };
-                codeDeploy.putLifecycleEventHookExecutionStatus(
-                    params,
-                    function(codeDeployError, codeDeployData){
-                        if (codeDeployError) {
-                            console.error(codeDeployError);
-                            callback('Validation test failed');
+                s3.upload(
+                    {
+                        ContentType: "application/xml",
+                        Bucket: resultsBucket,
+                        Body: testResultsData,
+                        Key: `/${environment}/Time.Api/${resultsFile}`
+                    },
+                    function (s3Error, s3Data) {
+                        if (s3Error) {
+                            console.error(s3Error);
+                        } else if (s3Data) {
+                            console.log(s3Data);
+                        } 
+                        
+                        if (newmanError) {
+                            const params = {
+                                deploymentId: event.DeploymentId,
+                                lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
+                                status: 'Failed'
+                            };
+                            codeDeploy.putLifecycleEventHookExecutionStatus(
+                                params,
+                                function (codeDeployError, codeDeployData) {
+                                    if (codeDeployError) {
+                                        console.error(codeDeployError);
+                                        callback('Validation test failed');
+                                    } else {
+                                        console.log(codeDeployData);
+                                        callback(null, 'Validation test succeeded');
+                                    }
+                                });
                         } else {
-                            console.log(codeDeployData);
-                            callback(null, 'Validation test succeeded');
-                        }
-                    });
-            } else {
-                console.log(data);
-                const params = {
-                    deploymentId: event.DeploymentId,
-                    lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
-                    status: 'Succeeded'
-                };
-                codeDeploy.putLifecycleEventHookExecutionStatus(
-                    params,
-                    function(codeDeployError, codeDeployData){
-                        if (codeDeployError) {
-                            console.error(codeDeployError);
-                            callback('Validation test failed');
-                        } else {
-                            console.log(codeDeployData);
-                            callback(null, 'Validation test succeeded');
+                            const params = {
+                                deploymentId: event.DeploymentId,
+                                lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
+                                status: 'Succeeded'
+                            };
+                            codeDeploy.putLifecycleEventHookExecutionStatus(
+                                params,
+                                function (codeDeployError, codeDeployData) {
+                                    if (codeDeployError) {
+                                        console.error(codeDeployError);
+                                        callback('Validation test failed');
+                                    } else {
+                                        console.log(codeDeployData);
+                                        callback(null, 'Validation test succeeded');
+                                    }
+                                });
                         }
                     });
             }
