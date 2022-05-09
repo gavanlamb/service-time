fd4e9b546cd0b9021696ecf55ae4ca554969a794
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-05-09 16:50:21 +1000

    Update load_tests to use lambda-jmeter tests

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -609,7 +609,7 @@ resource "aws_lambda_function" "load_tests" {
   package_type = "Image"
   publish = true
 
-  image_uri = "${data.aws_ecr_repository.lambda_postman.repository_url}:1.0.2381-1"
+  image_uri = "${data.aws_ecr_repository.load_tests.repository_url}:1.0.2381-1"
 
   memory_size = 10240
 
