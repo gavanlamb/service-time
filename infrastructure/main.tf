f049c9b2799f69f086113cd1095892ca225b9911
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-05-17 15:06:29 +1000

    Upgrade api tests container version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -546,7 +546,7 @@ resource "aws_lambda_function" "api_tests" {
   package_type = "Image"
   publish = true
 
-  image_uri = "${data.aws_ecr_repository.lambda_postman[0].repository_url}:1.0.2432-1"
+  image_uri = "${data.aws_ecr_repository.lambda_postman[0].repository_url}:1.0.2466.1"
 
   memory_size = 10240
   ephemeral_storage {
